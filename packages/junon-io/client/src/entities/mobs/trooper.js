9da7dea221eaf92062b891cdb2fc3f38546c7557
Pubertal Ulcer <kontrevolucia@gmail.com>
2026-06-20 18:40:26 +0400

    Update trooper.js

--- packages/junon-io/client/src/entities/mobs/trooper.js
+++ packages/junon-io/client/src/entities/mobs/trooper.js
@@ -3,7 +3,7 @@ const LandMob = require('./land_mob')
 const Constants = require("./../../../../common/constants.json")
 const Protocol = require("./../../../../common/util/protocol")
 
-class Trooper extends LandMob {
+class Trooper extends Guard {
 
   getConstantsTable() {
     return "Mobs.Trooper"
@@ -11,18 +11,6 @@ class Trooper extends LandMob {
 
   getType() {
     return Protocol.definition().MobType.Trooper
-  }
-
-  getSpritePath() {
-    return "trooper_active.png"
-  }
-
-  getCorpseSprite() {
-    let sprite = super.getCorpseSprite()
-    sprite.texture = PIXI.utils.TextureCache["trooper_corpse.png"]
-    sprite.height = 40
-
-    return sprite
   }
 
   getDizzySpriteConfig() {
