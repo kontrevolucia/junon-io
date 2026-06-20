9057e8d81b7d57696e35b2ead145bb94a28a6c89
Pubertal Ulcer <kontrevolucia@gmail.com>
2026-06-20 14:35:23 +0400

    Better SCAR-17

--- packages/junon-io/server/entities/equipments/hand/scar_17.js
+++ packages/junon-io/server/entities/equipments/hand/scar_17.js
@@ -1,13 +1,41 @@
-
 const HandEquipment = require("./hand_equipment")
 const Projectiles = require("./../../projectiles/index")
 
 const Protocol = require('../../../../common/util/protocol')
 const Constants = require("./../../../../common/constants.json")
-const AssaultRifle = require("./assault_rifle")
 
 
-class Scar17 extends AssaultRifle {
+class Scar17 extends HandEquipment {
+
+  use(user, targetEntity) {
+    if (!user.hasInfiniteAmmo()) {
+      const ammoType = this.getAmmoType()
+      const ammo = user.inventory.search(ammoType)
+      if (!ammo) {
+        user.showError("RifleAmmo Required")
+        return false
+      }
+
+      ammo.reduceCount(1)
+    }
+
+    super.use(user, targetEntity)
+
+    let sourcePoint = user.game.pointFromDistance(user.getX(), user.getY(), Constants.tileSize * 3 + 20, user.getRadAngle())
+
+    let angleInRad = user.getRadAngle()
+    let angleRandomizer = 2 - Math.floor(Math.random() * 5)
+    angleInRad = angleInRad + (angleRandomizer * Math.PI / 180)
+
+    Projectiles.RifleBullet.build({
+      weapon:        this,
+      source:      { x: sourcePoint[0],         y: sourcePoint[1] },
+      destination: user.getShootTarget(this, angleInRad)
+    })
+
+    return true
+  }
+
   getConstantsTable() {
     return "Equipments.Scar17"
   }
